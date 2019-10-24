48f1ac1849c03b3ea5bff66460b024232b879758
Holytu <nthuxy12345@gmail.com>
2019-10-24 18:09:59 +0800

    Build HW5

---
+++ OOP_HW5/test/ut_fs.h
@@ -0,0 +1,19 @@
+#ifndef UT_FS_H
+#define UT_FS_H
+
+#include <sys/stat.h>
+
+#include "../src/file.h"
+#include "../src/folder.h"
+#include "../src/iterator.h"
+#include "../src/node.h"
+#include "../src/null_iterator.h"
+#include "../src/utilities.h"
+
+using namespace std;
+TEST(FileTest, First)
+{
+    ASSERT_EQ(true, false);
+}
+
+#endif
